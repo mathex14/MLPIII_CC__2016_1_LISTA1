ffa19d94f60b126ebf92bc625bec6512cebd96f5
mathex14 <mathex14@gmail.com>
2016-02-24 14:42:28 -0300

    Update main_fatura.java

--- Questao-1/main_fatura.java
+++ Questao-1/main_fatura.java
@@ -4,7 +4,7 @@ public class main_fatura {
 	public static void main(String[] args){
 		Fatura fatura;
 		fatura = new Fatura();
-		System.out.println(fatura.getValorFatura(5, 10));
+		System.out.println("Fatura: " + fatura.getValorFatura(5, 10));
 	}
 
 }
